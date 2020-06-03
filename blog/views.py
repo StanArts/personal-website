711497dc1ea99bea552ebd740b4c303bbc9eebe3
StanArts <stanimirgevel@gmail.com>
2020-06-03 01:03:19 +0100

    Added more administrative options to manage the posts.

--- blog/views.py
+++ blog/views.py
@@ -25,7 +25,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now() // Removing that line allows us to save a post as a draft and review it later rahter then instantly publishing it.
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -39,9 +39,23 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date') #checks for unpublished posts and order them by date of creation
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
